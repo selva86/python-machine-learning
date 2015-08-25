d5e42a06a4d3070441e12522c4bf2303c3e04a85
Selva Prabhakaran <selva86@gmail.com>
2015-08-25 10:27:50 +0530

    Completed et classification for naive bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,10 +12,13 @@
 """
     
 import sys
+import os
 from time import time
-sys.path.append("../tools/")
+sys.path.append("/Users/selvaprabhakaran/Documents/work/pythonwork/python_ML_git/ud120-projects/tools/")
 from email_preprocess import preprocess
 
+# Set cwd
+os.chdir("/Users/selvaprabhakaran/Documents/work/pythonwork/python_ML_git/ud120-projects/tools/")
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -27,8 +30,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+clf = GaussianNB()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "Training Time: ", time()-t0, " secs"
 
+t0 = time()
+predicteds = clf.predict(features_test)
+print "Prediction Time: ", time()-t0, " secs"
+
+# Calc accuracy
+print "Accuracy score: ", accuracy_score(labels_test, predicteds), " %"
 #########################################################
 
 
