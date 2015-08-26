48503d5f1df79090bd68ec6a344834a62a9ee09f
Selva Prabhakaran <selva86@gmail.com>
2015-08-26 22:09:48 +0530

    Finished SVMproblems

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,7 +26,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+clf = SVC(kernel="linear")
+clf.fit(features_train, labels_train)
+preds = clf.predict(features_test)
 
+from sklearn.metrics import accuracy_score
+accuracy_score(labels_test, preds)
 #########################################################
 
 
